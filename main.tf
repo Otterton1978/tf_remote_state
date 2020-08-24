ecbc4e61859d1580d4eb9161d6ed7406284fc354
Otterton1978 <yohei@hashicorp.com>
2020-08-24 23:41:21 +1000

    Initial remote_state module

---
+++ main.tf
@@ -0,0 +1,18 @@
+provider "aws" {
+  region  = var.region
+}
+
+resource "aws_s3_bucket" "remote_state" {
+  bucket = "${var.prefix}-remote-state-${var.environment}"
+  acl    = "authenticated-read"
+  lifecycle {
+    prevent_destroy = true
+  }
+  versioning {
+    enabled = true
+  }
+  tags = {
+    Name        = "${var.prefix}-remote-state-${var.environment}"
+    Environment = var.environment
+  }
+}
